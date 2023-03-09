4aac10b0eca963ac605bfc873ce63b815e2f299c
Vladislav <galdinsh77@mail.ru>
2023-03-09 16:47:59 +0500

    Решили первую задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+ Console.Clear();
+ Console.Write("Введите 1-ое число: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите 2-ое число: ");
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a > b){ 
+     Console.WriteLine($"max = {a}");
+     Console.WriteLine($"min = {b}");
+ }
+ else if (a < b){
+     Console.WriteLine($"max= {b}");
+     Console.WriteLine($"min= {a}");
+ }
+ else
+     Console.WriteLine("Числа равны");
+
