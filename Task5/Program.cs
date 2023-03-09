1ee027fe19d6f99d3b76063e440ab35c0a76160c
Vladislav <galdinsh77@mail.ru>
2023-03-09 17:58:05 +0500

    Решили задачу №8

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), i = 0;
+n = Math.Abs(n);
+while (i <= n){
+if (i%2! == 0)
+    Console.Write($"{i} ");
+    i = i + 2;
+}
