56e2481bcf33e282018cb6cfa4660e8b6d330194
Vladislav <galdinsh77@mail.ru>
2023-03-09 17:34:00 +0500

    Решили задачу №4

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿ 
+ Console.Clear();
+ Console.Write("Введите 1-ое число: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите 2-ое число: ");
+ int b = Convert.ToInt32(Console.ReadLine());
+ Console.Write("Введите 3-eе число: ");
+ int c = Convert.ToInt32(Console.ReadLine());
+ int max = 0;
+ if (a > b)
+    max = a;
+ else if (b > c)
+    max = b;
+ else
+     max = c;
+Console.WriteLine($"max = {max}");
